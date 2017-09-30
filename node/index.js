e11db55ff703dd12507df3e7e4a490a104c8cf3a
Rohjay <ryan@wpengine.com>
2017-09-30 11:39:02 -0500

    Got the node app working with example index.html

--- node/index.js
+++ node/index.js
@@ -1,24 +1,33 @@
-
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
-app.use(express.static('ui'));
+var data = {
+    time: "updating",
+    moisture: "updating",
+    temp: "updaating",
+    ph: "updating"
+};
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/../frontend/build/index.html');
+});
+
+// example curl:
+// curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://localhost:3000/update
+app.post('/update', function(req, res) {
+    io.sockets.emit('update', req.body);
+    res.status(200).end();
 });
 
 io.on('connection', function(socket){
-    setInterval(
-        function(){
-            io.emit('rando', Math.floor(Math.random()*100));
-        },
-        500
-    );
+    socket.on('update', function(back_at_me){});
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+http.listen(3000, function(){});
+
